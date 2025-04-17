7bb3122c81b222aedc4167013567fbdb0369d4df
Dominik Adrian Grzywak <starterx4@gmail.com>
2025-04-18 00:16:40 +0200

    up COMPILE_ALL.sh compile script; clear compiled dir

--- addons/sourcemod/scripting/kether/COMPILE_ALL.sh
+++ addons/sourcemod/scripting/kether/COMPILE_ALL.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-for i in *.sp; do ./compile.sh $i; done
+errors=0
+for i in *.sp
+do ./compile.sh $i || {
+	errors=$((errors + 1))
+	continue
+}
+done
+if [ "$errors" -gt 0 ]; then
+	echo -e "\n$errors plugin(s) failed to compile."
+	exit 1
+else
+	echo -e "\nAll plugins compiled successfully."
+	exit 0
+fi
